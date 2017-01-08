1cfcf76ee0ed8d0f32901c7b66c0f59246afe084
Victor Macias <v.maciascastillo@gmail.com>
2017-01-08 16:45:53 +0100

    api & react

---
+++ tuparroquia-blog/app/assets/javascripts/components/banner.js.jsx
@@ -0,0 +1,18 @@
+var Banner = React.createClass({
+  render: function() {
+    var imgUrl = this.props.img_src;
+    var bannerStyle = {
+      backgroundImage: 'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), url('+imgUrl+')'
+    };
+
+    return(
+      <div className="banner" style={bannerStyle}>
+        <div className="banner-content">
+        <img src="assets/logo.png" alt="tuParroquia"/>
+          <h1>tuParroquia</h1>
+          <p>Gestiona las necesidades de tuParroquia en una sola plataforma</p>
+        </div>
+      </div>
+    )
+  }
+});
---
+++ tuparroquia-blog/app/assets/javascripts/components/footer.js.jsx
@@ -0,0 +1,35 @@
+var Footer = React.createClass({
+  render: function() {
+    return(
+    	<div className="footer">
+    	  <div className="container-fluid">
+    	    <div className="row">
+    	      <div className="col-xs-12 col-md-6">
+    	        <div className="links-socials">
+    	          <ul>
+    	            <li>
+    	              <a target="_blank" href="#">
+    	              <i className="fa fa-facebook"></i></a>
+    	            </li>
+    	            <li>
+    	              <a target="_blank" href="#">
+    	              <i className="fa fa-twitter"></i></a>
+    	            </li>
+    	            <li>
+    	              <a target="_blank" href="#">
+    	              <i className="fa fa-instagram"></i></a>
+    	            </li>
+    	          </ul>
+    	        </div>
+    	      </div>
+    	      <div className="col-xs-12 col-md-5">
+    	        <div className="copyrights">
+    	          <p>Made with <i className="fa fa-heart"></i> by Victor</p>
+    	        </div>
+    	      </div>
+    	    </div>
+    	  </div>
+    	</div>
+    )
+  }
+});
