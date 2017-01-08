ecd305715c128025379e98eec44e9bd00cde9bd8
Victor Macias <v.maciascastillo@gmail.com>
2017-01-08 17:29:07 +0100

    banner

--- tuparroquia-blog/app/assets/javascripts/components/banner.js.jsx
+++ tuparroquia-blog/app/assets/javascripts/components/banner.js.jsx
@@ -2,7 +2,7 @@ var Banner = React.createClass({
   render: function() {
     var imgUrl = this.props.img_src;
     var bannerStyle = {
-      backgroundImage: 'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), url('+imgUrl+')'
+      backgroundImage: 'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), image-url('+imgUrl+')'
     };
 
     return(
